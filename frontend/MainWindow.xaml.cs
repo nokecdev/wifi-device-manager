3e8647abbec6098119890db9eaa7b1c986a2a29c
bohdant8899 <bohdant8899@gmail.com>
2025-11-22 03:00:31 +0900

    remove not needed lines

--- frontend/MainWindow.xaml.cs
+++ frontend/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using frontend.ViewModels;
 
 namespace frontend
--- frontend/ViewModels/MainWindowViewModel.cs
+++ frontend/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,4 @@
-using System;
 using System.Collections.ObjectModel;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
